2639192319da77ed58b18c29b3bf3ab14e077cf6
rizerkrof <34008166+rizerkrof@users.noreply.github.com>
2022-12-07 15:29:24 +0100

    refacto

--- first_micro/main.py
+++ first_micro/main.py
@@ -1,20 +1,32 @@
-def app(event):
-    return "Hello, world!"
-
-from flask import Flask
-
+from flask import Flask, render_template
+import logging
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
-def hello_world():
-    prefix_google = """
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SSBHSGEKLF"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-SSBHSGEKLF');
-        </script>
-    """
-    return prefix_google + "Hello World"
+# def hello_world():
+#     prefix_google = """
+        # <!-- Google tag (gtag.js) -->
+        # <script async src="https://www.googletagmanager.com/gtag/js?id=G-SSBHSGEKLF"></script>
+        # <script>
+        #     window.dataLayer = window.dataLayer || [];
+        #     function gtag(){dataLayer.push(arguments);}
+        #     gtag('js', new Date());
+        #     gtag('config', 'G-SSBHSGEKLF');
+        # </script>
+#         <body>
+#             <button onclick="func()">Click me</button>
+#             <script src="googleAnalytics.js"></script>
+#         </body>
+#     """
+#     return prefix_google + "Hello World"
+def home():
+    return render_template('home.html')
+
+@app.route('/logger', methods=['GET'])
+def logger():
+    loggerLog = 'You entered logger page'
+    app.logger.info(loggerLog)
+    return render_template('logger.html', value=loggerLog)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ first_micro/static/googleAnalytics.js
@@ -0,0 +1,14 @@
+function googleAnalyticsTraffic(){
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SSBHSGEKLF');
+}
+
+function func(){
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SSBHSGEKLF');
+    alert('you generated a Google analytics event!');
+}
